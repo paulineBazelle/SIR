ef7b312e6c0569c327583ccfc786742ec42c627d
pbazelle <pauline.bazelle@gmail.com>
2017-01-04 11:40:33 +0100

    modifications de agent et sir

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 
 class Agent :
 
-	def __init__(self,etat): 
+	def __init__(self,etat,x,y): 
 		self.etat=etat
+		self.x=x
+		self.y=y
 	
 	def __str__(self):
 		return str(self.etat)
--- sir.py
+++ sir.py
@@ -1,23 +1,30 @@
 from agent import Agent
 import random
 
-def initialisation(w, h,n):
-		tab=[[0]*h]*w
-		for i in range(w):
-			for j in range(h):
-				tab[i][j]= []
+def initialisation(w, h,n, num_simul, pr, pm, pi):
+		#tab=[[0]*h]*w
+		#for i in range(w):
+		#	for j in range(h):
+		#		tab[i][j]= []
+		f= open("donnees.txt","w")
+		f.write("Simulation " + str(num_simul) + "\n")
+		f.write(str(w) +" "+ str(h) +" " + str(n) + " "+ str(pr) + " " + str(pm) + " " + str(pi) +"\n")
+		f.write("0" + "\n")
+		f.write("Initialisation" +"\n")
 		for k in range(n-1):
 			x=random.randint(0,w-1)
 			y=random.randint(0,h-1)
-			tab[x][y].append(Agent(0))
+			f.write(str(x) +" " + str(y) + " 0\n")
+			#tab[x][y].append(Agent(0))
 		x=random.randint(0,w-1)
 		y=random.randint(0,h-1)
-		tab[x][y].append(Agent(1))
-		return tab 
+		f.write(str(x) +" " + str(y) + " 1\n")
+		#tab[x][y].append(Agent(1))
+		f.close()
 		
 		
 		
-print (initialisation(10,5,5))
+initialisation(10,5,5,1,0.5,0.4,0.1)
 
 	
 	
