2732c117054b3762dfdc0e2286905148b947d431
Elea Greugny <elea.greugny@insa-lyon.fr>
2017-01-28 12:45:00 +0100

    debug serveur

--- serveur.py
+++ serveur.py
@@ -4,6 +4,7 @@ from time import time, ctime
 import sys
 import signal
 from simulation import Simulation
+import errno
 
 stopBoolServ = True
 
@@ -48,66 +49,75 @@ class Serveur:
     receptionFichier = False
     sockClient.send('Action')
     while again:
-      data = sockClient.recv(self.TAILLE_BLOC)
-      print('recu : %s' %data)
-      if receptionFichier:
-        print "Telechargement fichier: ",data
-        sockClient.send('end')
-        fichier=open("serveur.txt","w")
-        data = data.split('\n')
-        if data[0] == 'Create':
-          data[0] = len(self.simulations)
-          simul = Simulation(len(self.simulations))
-          self.simulations.append(simul)
+      try:
+        data = sockClient.recv(self.TAILLE_BLOC)
+        print('recu : %s' %data)
+        if receptionFichier:
+          print "Telechargement fichier: ",data
+          sockClient.send('end')
+          fichier=open("serveur.txt","w")
+          data = data.split('\n')
+          if data[0] == 'Create':
+            data[0] = str(len(self.simulations))
+            simul = Simulation(len(self.simulations))
+            self.simulations.append(simul)
+          else:
+            simul_courante = self.simulations[int(data[0])]
+            etape = data[8]
+            if etape == 'Move':
+              print('end move')
+              simul_courante.infect = True
+              simul_courante.move = False
+            if etape == 'Infect':
+              print('end infect')
+              simul_courante.infect = False
+              simul_courante.update = True
+            if etape == 'Update':
+              print('end update')
+              simul_courante.update = False
+              simul_courante.stats = True
+            if etape == 'Stats':
+              print('end stats')
+              simul_courante.stats = False
+              pas = int(data[7])
+              if pas == self.nbPas:
+                simul_courante.statsFinale()
+              if pas < self.nbPas:
+                print('end pas %i' %pas)
+                simul_courante.finPas()
+                data[7] = str(pas + 1)
+                agents = []
+                for line in data[9:13]:
+                  agents.append(int(line.split()[1]))
+                simul_courante.S = agents[0]
+                simul_courante.I = agents[1]
+                simul_courante.R = agents[2]
+                simul_courante.M = agents[3]
+                del data[9:13]
+          for line in data:
+            fichier.write(line + '\n')
+          #fichier.writelines(data)
+          fichier.close()
+          receptionFichier = False
         else:
-          simul_courante = self.simulations[data[0]]
-          etape = data[8]
-          if etape == 'Move':
-            print('end move')
-            simul_courante.infect = True
-            simul_courante.move = False
-          if etape == 'Infect':
-            print('end infect')
-            simul_courante.infect = False
-            simul_courante.update = True
-          if etape == 'Update':
-            print('end update')
-            simul_courante.update = False
-            simul_courante.stats = True
-          if etape == 'Stats':
-            print('end stats')
-            simul_courante.stats = False
-            pas = int(data[7])
-            if pas == self.nbPas:
-              simul_courante.statsFinale()
-            if pas < self.nbPas:
-              print('end pas %i' %pas)
-              simul_courante.finPas()
-              data[7] = str(pas + 1)
-              agents = []
-              for line in data[9:13]:
-                agents.append(int(line.split()[1]))
-              simul_courante.S = agents[0]
-              simul_courante.I = agents[1]
-              simul_courante.R = agents[2]
-              simul_courante.M = agents[3]
-              del data[9:13]
-        fichier.writelines(data)
-        fichier.close()
-      else:
-        if data == 'Envoi':
-          print("Reception d'un fichier")
-          receptionFichier = True
-          sockClient.send('Pret')
-        if data == 'Pret':
-          print("Envoi d'un fichier")
-          f=open("serveur.txt","r")
-          data=f.read()
-          sockClient.send(data)
-          f.close()
-        if data == "end":
-          print "fin de la connexion demandee par le client"
-          again = False
+          if data == 'Envoi':
+            print("Reception d'un fichier")
+            receptionFichier = True
+            sockClient.send('Pret')
+          if data == 'Pret':
+            print("Envoi d'un fichier")
+            f=open("serveur.txt","r")
+            data=f.read()
+            sockClient.send(data)
+            f.close()
+          if data == "end":
+            print "fin de la connexion demandee par le client"
+            again = False
+      
+      except IOError,e:
+        if e.errno == errno.ECONNRESET:
+          print('possible perte de donnees...')
+  
     print('fin de la communication')
     sockClient.shutdown(1)
     sockClient.close()
