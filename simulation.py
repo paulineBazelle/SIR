e3feb68f1c44df073a2993870501845e5988cb69
justinebosc <justine.bosc@insa-lyon.fr>
2017-01-29 16:56:33 +0100

    modification stat finale

--- simulation.py
+++ simulation.py
@@ -38,13 +38,15 @@ class Simulation:
   
   def statsFinale(self):
     t=np.arange(self.pas)
+    print ("Sains :", self.S)
     plt.plot(t,self.S,c="green")
-    plt.hold(True)
+    #plt.hold(True)
     plt.plot(t,self.I,c="blue")
-    plt.hold(True)
-    plt.plot(t,self.R,c="yellow")
-    plt.hold(True)
+    #plt.hold(True)
+    plt.plot(t,self.R,c="orange")
+    #plt.hold(True)
     plt.plot(t,self.M,c="red")
+    plt.axis([0, self.pas, 0, (self.S[0]+self.I[0]+self.R[0]+self.M[0])])
     plt.savefig('statistics',format='png') 
     plt.show()
   
